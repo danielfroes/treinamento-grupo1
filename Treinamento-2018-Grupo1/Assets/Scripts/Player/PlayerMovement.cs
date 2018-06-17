a62b468168a68bc10f39250d2beda968ab68c6c2
Unknown <gustavosasakironcaglia@usp.br>
2018-06-17 17:13:55 -0300

    Arrumei o erro da soma de addForces

--- Treinamento-2018-Grupo1/Assets/Scripts/Player/PlayerMovement.cs
+++ Treinamento-2018-Grupo1/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,15 +7,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour {
 	
-	public float jForce;//força do pulo
-	public float hForce;//força horizontal, andar
-	//public float hVel;//velocidade caso mude de direcao ou 
+	public float alturaPulo;//altura maxima do pulo
+	private float gravidade;
+	public float hAce;//aceleracao horizontal(andar)
 	public float hMax;//velocidade horizontal maxima
 
 	public Player target;// Player que recebe o input desse script.
 	public Transform checarChao;//ponto o qual é checado se colide com o chao
+	public Transform checarChao2;//ponto o qual é checado se colide com o chao
 	private Rigidbody2D rigi;//rigidbody do target
 	private bool grounded;//verifica se está tocando no solo
+
+	private float hNewVel;//nova velocidade horizontal do player
+	private float vNewVel;//nova velocidade vertical do player
 
 	void Start () {
 
@@ -24,31 +28,34 @@ public class PlayerMovement : MonoBehaviour {
             target = GetComponent<Player>();
 		
 		rigi = target.GetComponent<Rigidbody2D>();
+		gravidade =rigi.gravityScale;
     }
 
 	void FixedUpdate () {
+
 		//movimento horizontal
-	//	if(!(rigi.velocity.x > 0) && target.input.horizontalAxis >0){
-	//		rigi.velocity = new Vector2(hVel,0);
-	//	}else if(!(rigi.velocity.x < 0) && target.input.horizontalAxis <0){
-	//		rigi.velocity = new Vector2(-hVel,0);
-	//	}
-
-		rigi.AddForce( transform.right * hForce * target.input.horizontalAxis );
+		hNewVel = hAce*target.input.horizontalAxis + rigi.velocity.x;
+		vNewVel = rigi.velocity.y;
 		
-
 		//atingindo velocidade maxima
 		if(Mathf.Abs (rigi.velocity.x) > hMax)
-			rigi.velocity = new Vector2(Mathf.Sign (rigi.velocity.x) * hMax, rigi.velocity.y);
+			hNewVel = Mathf.Sign (rigi.velocity.x) * hMax;
 		
 
-		
 		//verificando que esta tocando no chao
-		grounded = Physics2D.Linecast(transform.position, checarChao.position, 1 << LayerMask.NameToLayer("Ground"));
+		grounded = Physics2D.Linecast(transform.position, checarChao.position, 1 << LayerMask.NameToLayer("Ground")) ||
+		Physics2D.Linecast(transform.position, checarChao2.position, 1 << LayerMask.NameToLayer("Ground"));
 
-		//Pulo
-		if(target.input.jumpButton && grounded)
-			rigi.AddForce(transform.up * jForce);	
+		//evitando a soma de addForces no pulo
+		if(target.input.jumpButton && grounded){
+			vNewVel = 0;	
+		}
+
+		rigi.velocity = new Vector2(hNewVel,vNewVel);
 		
+		//Pulo
+		if(target.input.jumpButton && grounded){
+			rigi.AddForce(Mathf.Sqrt(2*alturaPulo*gravidade)*Vector2.up,ForceMode2D.Impulse);	
+		}
 	}
 }
